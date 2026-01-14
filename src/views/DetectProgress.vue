8fd8bc0587055e4909f02aa5e99252ff8aabcbbe
fengqing <fengqing@yourangroup.com>
2026-01-14 14:36:54 +0800

    [fix] 修复因为`setInterval`活跃，`npm run docs:build`进程不能退出的bug

--- src/views/DetectProgress.vue
+++ src/views/DetectProgress.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 const detectStates = ref([
   { name: '来料', state: 'end', progress: 0, info: '' },
   { name: '定位', state: 'end', progress: 0, info: '' },
@@ -8,13 +8,16 @@ const detectStates = ref([
   { name: '下料', state: 'wait', progress: 0, info: '' },
 ]);
 
-const timer = setInterval(() => {
-  detectStates.value[2].progress += 1;
-  if (detectStates.value[2].progress < 100)  return;
+// 因为`setInterval`活跃，直接写入最顶层`npm run docs:build`进程不能退出`
+onMounted(() => {
+  const timer = setInterval(() => {
+    detectStates.value[2].progress += 1;
+    if (detectStates.value[2].progress < 100) return;
 
-  clearInterval(timer);
-  detectStates.value[2].state = 'end';
-}, 500)
+    clearInterval(timer);
+    detectStates.value[2].state = 'end';
+  }, 500)
+})
 </script>
 <template>
   <div class="m20" style="width: 500px;">
--- src/views/DetectStat.vue
+++ src/views/DetectStat.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 const totalCount = ref(0);
 const okCount = ref(0);
 const totalChipCount = ref(0);
@@ -13,14 +13,17 @@ function clearCB() {
 }
 
 let lastTime = Date.now();
-setInterval(() => {
-  totalCount.value++;
-  okCount.value += Math.random() > 0.1 ? 1 : 0;
-  totalChipCount.value += 1024;
-  okChipCount.value += 1000 + ~~(Math.random() * 24);
-  time.value = (Date.now() - lastTime) / 1000
-  lastTime = Date.now()
-}, 1000)
+// 因为`setInterval`活跃，直接写入最顶层`npm run docs:build`进程不能退出`
+onMounted(() => {
+  setInterval(() => {
+    totalCount.value++;
+    okCount.value += Math.random() > 0.1 ? 1 : 0;
+    totalChipCount.value += 1024;
+    okChipCount.value += 1000 + ~~(Math.random() * 24);
+    time.value = (Date.now() - lastTime) / 1000
+    lastTime = Date.now()
+  }, 1000)
+})
 </script>
 <template>
   <div style="width: 1290px; height:196px;">
